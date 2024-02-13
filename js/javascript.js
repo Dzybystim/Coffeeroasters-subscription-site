761b34d446674492ee1c40886dcc09556f516140
Dima <103567184+Dzybystim@users.noreply.github.com>
2024-02-13 16:52:18 +0200

    js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,13 @@
-const number = 6;
+const message = 5;
+console.log(message);
 
-console.log(number);
+const isComing = confirm("Please confirm hotel reservation");
+console.log(isComing);
+
+const hotelName = prompt("Please enter desired hotel name");
+console.log(hotelName);
+
+const max = 10;
+for (let i = 0; i < max; i += 1) {
+  console.log(`${max} % ${i} = `, max % i);
+}
